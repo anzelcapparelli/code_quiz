3f94d3f5be73ba0c9a09c976704284ace2e9e474
Anzel Capparelli <anzelcapparelli@gmail.com>
2020-12-14 08:55:09 -0800

    added an array of arrays to store questions; playing with a thrid layer to assign answer choices boolean (allows to randomize answer order)

--- script.js
+++ script.js
@@ -4,25 +4,44 @@
 // h1El.textContent="Test";
 
 var time_remaining = 0;
-
-
-var divEl=document.createElement("div");
 var show_time = document.querySelector(".timer");
-show_time.appendChild(divEl);
+var questions = [
+    ["Q1 This is where the question will go",
+        ["answr 1", true], ["answr 2", false], ["answr 3", false], ["answr 4", false]],
+    ["Q2 This is where the question will go",
+        ["answr 1", true], ["answr 2", false], ["answr 3", false], ["answr 4", false]],
+    ["Q3 This is where the question will go",
+        ["answr 1", true], ["answr 2", false], ["answr 3", false], ["answr 4", false]],
+    ["Q4 This is where the question will go",
+        ["answr 1", true], ["answr 2", false], ["answr 3", false], ["answr 4", false]],
+    ["Q5 This is where the question will go",
+        ["answr 1", true], ["answr 2", false], ["answr 3", false], ["answr 4", false]],
+
+]
+
+//In question box, show questions[Q Index][0]
+//In possible answers, show quesions[Same index][1-4][0] use for loop!
+//when they click, have it ref questions[Same index][chosen index][2] to det true/false
+// ^store click as a event.target maybe? don't want to do an if for each index (maybe do it to start though!)
+
+    
+// declare functions logically (chronological)! Will help visualize
+
+function time_start() {
+    time_remaining = 76;        //bc first action in interval is --, fisrt number shown is 75, and properly shows 1 instead of cutting off
+    timer();
+}
 
 function timer() {
     var time_left = setInterval(function () {
-        divEl.textContent = time_remaining;
         time_remaining--;
-        if (time_left <= 0) {
+        show_time.textContent = "Timer: " + time_remaining;
+        if (time_remaining <= 0) {
+            show_time.textContent = "Timer:__ ";
             clearInterval(time_left);
         }
     }, 1000)
 }
 
-function time_start() {
-    time_remaining = 75;
-    timer();
-}
 
-time_start;
+time_start();
