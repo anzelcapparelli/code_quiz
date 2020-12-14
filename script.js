2fe6d009ec97a2e9291472e3ddb1738edbac7ef8
Anzel Capparelli <anzelcapparelli@gmail.com>
2020-12-14 00:01:28 -0800

    included framework for timer to count down; working on getting timer to show on webpage

--- script.js
+++ script.js
@@ -2,3 +2,27 @@
 // document.body.appendChild(h1El);
 
 // h1El.textContent="Test";
+
+var time_remaining = 0;
+
+
+var divEl=document.createElement("div");
+var show_time = document.querySelector(".timer");
+show_time.appendChild(divEl);
+
+function timer() {
+    var time_left = setInterval(function () {
+        divEl.textContent = time_remaining;
+        time_remaining--;
+        if (time_left <= 0) {
+            clearInterval(time_left);
+        }
+    }, 1000)
+}
+
+function time_start() {
+    time_remaining = 75;
+    timer();
+}
+
+time_start;
